2c175ad492e3da4526afda2e2598f4fb0ad5502e
Batuhan Yerlikaya <batuhanyky@hotmail.com>
2023-01-22 19:01:39 +0300

    feat: Taş Kağıt Makas uygulaması yapıldı

---
+++ tas-kagit-makas.js
@@ -0,0 +1,49 @@
+let appKutu = document.querySelector("#app")
+
+let oyuncuBirKutu = document.createElement("div")
+let oyuncuIkiKutu = document.createElement("div")
+let oynaButonu = document.createElement("button")
+let oyunSayac = document.createElement("span")
+
+let oyunSayacAdet = 0;
+
+function rastgeleSayi(minimum, maksimum) {
+    return Math.floor( Math.random() * maksimum ) + minimum
+}
+
+function sonucUret() {
+    let sonuc = rastgeleSayi(1, 3)
+
+    return sonuc
+}
+
+function sahneOlustur() {
+    appKutu.append(oyuncuBirKutu, oyuncuIkiKutu, oynaButonu, oyunSayac)
+    oynaButonu.textContent = "Oyna"
+    oynaButonu.classList.add("btn", "btn-success")
+
+    oynaButonu.addEventListener("click", oyna)
+
+    oyuncuBirKutu.classList.add( "bg-primary", "p-5", "h1" )
+    oyuncuIkiKutu.classList.add( "bg-secondary", "p-5", "h1")
+    oyunSayac.classList.add("badge", "bg-info")
+}
+
+function oyna() {
+    let birinciOyuncuSonuc = sonucUret()
+    let ikinciOyuncuSonuc = sonucUret()
+    
+    if(birinciOyuncuSonuc === 1) birinciOyuncuSonuc = "Taş"
+    if(birinciOyuncuSonuc === 2) birinciOyuncuSonuc = "Kağıt"
+    if(birinciOyuncuSonuc === 3) birinciOyuncuSonuc = "Makas"
+    
+    ikinciOyuncuSonuc = ikinciOyuncuSonuc === 1 ? "Taş" :  ikinciOyuncuSonuc === 2 ? "Kağıt" : "Makas"
+
+    oyuncuBirKutu.textContent = birinciOyuncuSonuc
+    oyuncuIkiKutu.textContent = ikinciOyuncuSonuc
+
+    oyunSayacAdet = oyunSayacAdet + 1
+    oyunSayac.textContent = oyunSayacAdet
+}
+
+sahneOlustur()
